2018f1b1ad8a3c1dc21b6b8147b718fc58cb52b0
Gernot Ploiner <gp@webprofil.at>
2015-11-18 10:47:04 +0100

    fixed version dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	 'version' => '1.1.0',
 	 'constraints' => array(
 		  'depends' => array(
-				'typo3' => '6.2.0-6.2.99, 7.6.0-7.6.99',
+				'typo3' => '6.2.0-7.6.99',
 		  ),
 		  'conflicts' => array(),
 		  'suggests' => array(
