8c33420c9dc55457b4c48611a7062f5f53ceb51c
butu <bb@webprofil.at>
2016-03-03 11:24:02 +0100

    Fixed bug with page properties #70271

--- Classes/ViewHelpers/TcaViewHelper.php
+++ Classes/ViewHelpers/TcaViewHelper.php
@@ -35,28 +35,46 @@ class TcaViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
     public function render($type, $table)
     {
         $this->utility = new \MASK\Mask\Utility\MaskUtility($this->objectManager);
-        $forbiddenFields = array(
-            'starttime', 'endtime', 'hidden', 'sectionIndex', 'linkToTop', 'fe_group',
-            'CType', 'doktype', 'title', 'TSconfig', 'php_tree_stop', 'storage_pid',
-            'tx_impexp_origuid', 't3ver_label', 'editlock', 'url_scheme',
-            'extendToSubpages', 'nav_title', 'nav_hide', 'subtitle', 'target', 'alias',
-            'url', 'urltype', 'lastUpdated', 'newUntil', 'cache_timeout', 'cache_tags',
-            'no_cache', 'no_search', 'shortcut', 'shortcut_mode', 'content_from_pid',
-            'mount_pid', 'keywords', 'description', 'abstract', 'author',
-            'author_email', 'is_siteroot', 'mount_pid_ol', 'module', 'fe_login_mode',
-            'l18n_cfg', 'backend_layout', 'backend_layout_next_level'
-        );
-        foreach ($GLOBALS['TCA'][$table]['columns'] as $tcaField => $tcaConfig) {
-            $fieldType = $this->utility->getFormType($tcaField, "", $table);
-            if (
-                ($fieldType == $type ||
-                ($fieldType == "Text" &&
-                ($type == "Text" || $type == "Richtext")
-                )) && array_search($tcaField, $forbiddenFields) === FALSE
-            ) {
-                $temp["field"] = $tcaField;
-                $temp["label"] = $tcaConfig["label"];
-                $content[] = $temp;
+        // in tt_content allow all fields except $forbiddenFields
+        if ($table == "tt_content") {
+            $forbiddenFields = array(
+                'starttime', 'endtime', 'hidden', 'sectionIndex', 'linkToTop', 'fe_group',
+                'CType', 'doktype', 'title', 'TSconfig', 'php_tree_stop', 'storage_pid',
+                'tx_impexp_origuid', 't3ver_label', 'editlock', 'url_scheme',
+                'extendToSubpages', 'nav_title', 'nav_hide', 'subtitle', 'target', 'alias',
+                'url', 'urltype', 'lastUpdated', 'newUntil', 'cache_timeout', 'cache_tags',
+                'no_cache', 'no_search', 'shortcut', 'shortcut_mode', 'content_from_pid',
+                'mount_pid', 'keywords', 'description', 'abstract', 'author',
+                'author_email', 'is_siteroot', 'mount_pid_ol', 'module', 'fe_login_mode',
+                'l18n_cfg', 'backend_layout', 'backend_layout_next_level'
+            );
+            foreach ($GLOBALS['TCA'][$table]['columns'] as $tcaField => $tcaConfig) {
+                $fieldType = $this->utility->getFormType($tcaField, "", $table);
+                if (
+                    ($fieldType == $type ||
+                    ($fieldType == "Text" &&
+                    ($type == "Text" || $type == "Richtext")
+                    )) && array_search($tcaField, $forbiddenFields) === FALSE
+                ) {
+                    $temp["field"] = $tcaField;
+                    $temp["label"] = $tcaConfig["label"];
+                    $content[] = $temp;
+                }
+            }
+        } else {
+            // in pages allow only fields already created by mask
+            foreach ($GLOBALS['TCA'][$table]['columns'] as $tcaField => $tcaConfig) {
+                $fieldType = $this->utility->getFormType($tcaField, "", $table);
+                if (
+                    ($fieldType == $type ||
+                    ($fieldType == "Text" &&
+                    ($type == "Text" || $type == "Richtext")
+                    )) && strrpos($tcaField, "tx_mask_", -strlen($tcaField)) !== false
+                ) {
+                    $temp["field"] = $tcaField;
+                    $temp["label"] = $tcaConfig["label"];
+                    $content[] = $temp;
+                }
             }
         }
         return $content;
