c36ce4793aef40f97599699cf173c79a24ebeab8
butu <bb@webprofil.at>
2016-03-29 13:35:20 +0200

    Changed Version to 2.0.0dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,10 +16,10 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.1.4dev',
+    'version' => '2.0.0dev',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '7.6.0-7.6.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
