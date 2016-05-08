e21e5fa5f4047d45362035a12ecbe19764daf37e
butu <ichbingenial@gmail.com>
2016-05-08 23:11:13 +0200

    Added if viewhelper in html code generator

--- Classes/CodeGenerator/HtmlCodeGenerator.php
+++ Classes/CodeGenerator/HtmlCodeGenerator.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace MASK\Mask\CodeGenerator;
+<?php namespace MASK\Mask\CodeGenerator;
 
 /* * *************************************************************
  *  Copyright notice
@@ -34,106 +32,128 @@ namespace MASK\Mask\CodeGenerator;
 class HtmlCodeGenerator extends \MASK\Mask\CodeGenerator\AbstractCodeGenerator
 {
 
-    /**
-     * Generates Fluid HTML for Contentelements
-     *
-     * @param string $key
-     * @return string $html
-     * @author Gernot Ploiner <gp@webprofil.at>
-     *
-     */
-    public function generateHtml($key, $table = "tt_content")
-    {
-        $storage = $this->storageRepository->loadElement('tt_content', $key);
-        $html = "";
-        if ($storage["tca"]) {
-            foreach ($storage["tca"] as $fieldKey => $fieldConfig) {
-                $html .= $this->generateFieldHtml($fieldKey, $key);
-            }
-        }
-        return $html;
-    }
+   /**
+	* Generates Fluid HTML for Contentelements
+	*
+	* @param string $key
+	* @return string $html
+	* @author Gernot Ploiner <gp@webprofil.at>
+	*
+	*/
+   public function generateHtml($key, $table = "tt_content")
+   {
+	  $storage = $this->storageRepository->loadElement('tt_content', $key);
+	  $html = "";
+	  if ($storage["tca"]) {
+		 foreach ($storage["tca"] as $fieldKey => $fieldConfig) {
+			$html .= $this->generateFieldHtml($fieldKey, $key);
+		 }
+	  }
+	  return $html;
+   }
 
-    /**
-     * Generates HTML for a field
-     * @param string $fieldKey
-     * @param string $elementKey
-     * @param string $table
-     * @param string $datafield
-     * @return string $html
-     * @author Gernot Ploiner <gp@webprofil.at>
-     * @author Benjamin Butschell <bb@webprofil.at>
-     */
-    protected function generateFieldHtml($fieldKey, $elementKey, $table = "tt_content", $datafield = "data")
-    {
-        $html = "";
-        $fieldHelper = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('MASK\\Mask\\Helper\\FieldHelper');
-        switch ($fieldHelper->getFormType($fieldKey, $elementKey, $table)) {
-            case "Check":
-                $html .= "{f:if(condition: " . $datafield . "." . $fieldKey . ", then: 'On', else: 'Off')}<br />\n\n";
-                break;
-            case "Content": // TODO: Benjamin, Fluid-Vorlage für Feld "Content Verbindung":
-                $html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n\n";
-                break;
-            case "Date":
-                $html .= '<f:format.date format="d.m.Y">{' . $datafield . '.' . $fieldKey . '}</f:format.date><br />' . "\n\n";
-                break;
-            case "Datetime":
-                $html .= '<f:format.date format="d.m.Y - H:i:s">{' . $datafield . '.' . $fieldKey . '}</f:format.date><br />' . "\n\n";
-                break;
-            case "File":
-                $html .= '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="file">
+   /**
+	* Generates HTML for a field
+	* @param string $fieldKey
+	* @param string $elementKey
+	* @param string $table
+	* @param string $datafield
+	* @return string $html
+	* @author Gernot Ploiner <gp@webprofil.at>
+	* @author Benjamin Butschell <bb@webprofil.at>
+	*/
+   protected function generateFieldHtml($fieldKey, $elementKey, $table = "tt_content", $datafield = "data")
+   {
+	  $html = "";
+	  $fieldHelper = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('MASK\\Mask\\Helper\\FieldHelper');
+	  switch ($fieldHelper->getFormType($fieldKey, $elementKey, $table)) {
+		 case "Check":
+			$html .= "{f:if(condition: " . $datafield . "." . $fieldKey . ", then: 'On', else: 'Off')}<br />\n\n";
+			break;
+		 case "Content": // TODO: Benjamin, Fluid-Vorlage für Feld "Content Verbindung":
+			$html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n\n";
+			break;
+		 case "Date":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:format.date format="d.m.Y">{' . $datafield . '.' . $fieldKey . '}</f:format.date><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Datetime":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:format.date format="d.m.Y - H:i:s">{' . $datafield . '.' . $fieldKey . '}</f:format.date><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "File":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="file">
   <f:image src="{file.uid}" alt="{file.alternative}" title="{file.title}" treatIdAsReference="1" width="200" /><br />
   {file.description} / {file.identifier}<br />
-</f:for>' . "\n\n";
-                break;
-            case "Float":
-                $html .= '<f:format.number decimals="2" decimalSeparator="," thousandsSeparator=".">{' . $datafield . '.' . $fieldKey . '}</f:format.number><br />' . "\n\n";
-                break;
-            case "Inline":
-                $html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
-                $html .= "<ul>\n";
-                $html .= "<f:for each=\"{" . $datafield . "." . $fieldKey . "}\" as=\"" . $datafield . "_item" . "\">\n<li>";
-                $inlineFields = $this->storageRepository->loadInlineFields($fieldKey);
-                if ($inlineFields) {
-                    foreach ($inlineFields as $inlineField) {
-                        $html .= $this->generateFieldHtml($inlineField["maskKey"], $elementKey, $fieldKey, $datafield . "_item") . "\n";
-                    }
-                }
-                $html .= "</li>\n</f:for>" . "\n";
-                $html .= "</ul>\n";
-                $html .= "</f:if>\n\n";
-                break;
-            case "Integer":
-                $html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n\n";
-                break;
-            case "Link":
-                $html .= '<f:link.page pageUid="{' . $datafield . '.' . $fieldKey . '}">{data.' . $fieldKey . '}</f:link.page><br />' . "\n\n";
-                break;
-            case "Radio":
-                $html .= '<f:switch expression="{' . $datafield . '.' . $fieldKey . '}">
+</f:for>' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Float":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:format.number decimals="2" decimalSeparator="," thousandsSeparator=".">{' . $datafield . '.' . $fieldKey . '}</f:format.number><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Inline":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= "<ul>\n";
+			$html .= "<f:for each=\"{" . $datafield . "." . $fieldKey . "}\" as=\"" . $datafield . "_item" . "\">\n<li>";
+			$inlineFields = $this->storageRepository->loadInlineFields($fieldKey);
+			if ($inlineFields) {
+			   foreach ($inlineFields as $inlineField) {
+				  $html .= $this->generateFieldHtml($inlineField["maskKey"], $elementKey, $fieldKey, $datafield . "_item") . "\n";
+			   }
+			}
+			$html .= "</li>\n</f:for>" . "\n";
+			$html .= "</ul>\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Integer":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Link":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:link.page pageUid="{' . $datafield . '.' . $fieldKey . '}">{data.' . $fieldKey . '}</f:link.page><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Radio":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:switch expression="{' . $datafield . '.' . $fieldKey . '}">
   <f:case value="1">Value is: 1</f:case>
   <f:case value="2">Value is: 2</f:case>
   <f:case value="3">Value is: 3</f:case>
-</f:switch><br />' . "\n\n";
-                break;
-            case "Richtext":
-                $html .= '<f:format.html parseFuncTSPath="lib.parseFunc_RTE">{' . $datafield . '.' . $fieldKey . '}</f:format.html><br />' . "\n\n";
-                break;
-            case "Select":
-                $html .= '<f:switch expression="{' . $datafield . '.' . $fieldKey . '}">
+</f:switch><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Richtext":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:format.html parseFuncTSPath="lib.parseFunc_RTE">{' . $datafield . '.' . $fieldKey . '}</f:format.html><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Select":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:switch expression="{' . $datafield . '.' . $fieldKey . '}">
   <f:case value="1">Value is: 1</f:case>
   <f:case value="2">Value is: 2</f:case>
   <f:case value="3">Value is: 3</f:case>
-</f:switch><br />' . "\n\n";
-                break;
-            case "String":
-                $html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n\n";
-                break;
-            case "Text":
-                $html .= '<f:format.nl2br>{' . $datafield . '.' . $fieldKey . '}</f:format.nl2br><br />' . "\n\n";
-                break;
-        }
-        return $html;
-    }
+</f:switch><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "String":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '{' . $datafield . '.' . $fieldKey . '}<br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+		 case "Text":
+			$html .= '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">' . "\n";
+			$html .= '<f:format.nl2br>{' . $datafield . '.' . $fieldKey . '}</f:format.nl2br><br />' . "\n";
+			$html .= "</f:if>\n\n";
+			break;
+	  }
+	  return $html;
+   }
 }
