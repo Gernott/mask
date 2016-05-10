5a1a37d8afadcd205071093ae9f5639420724545
Benjamin Butschell <ichbingenial@gmail.com>
2016-05-10 16:22:16 +0200

    Updated contact data in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Mask',
-    'description' => 'Create your own content elements and page templates. Easy to use, even without programming skills because of the comfortable drag&drop system. Stored in structured database tables. Style your frontend with Fluid tags. Ideal, if you want to switch from Templavoila and create an advancedCE (FCE).',
+    'description' => 'Create your own content elements and page templates. Easy to use, even without programming skills because of the comfortable drag&drop system. Stored in structured database tables. Style your frontend with Fluid tags. Ideal, if you want to switch from Templavoila.',
     'category' => 'plugin',
-    'author' => 'TYPO3.experten Team',
-    'author_email' => 'office@typo3experten.com',
+    'author' => 'WEBprofil - Gernot Ploiner e.U.',
+    'author_email' => 'office@webprofil.at',
     'author_company' => 'WEBprofil - Gernot Ploiner e.U.',
     'shy' => '',
     'priority' => '',
