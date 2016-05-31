8a2b3c457d352a1339b3460cf26809c640cece6a
butu <bb@webprofil.at>
2016-05-31 14:11:41 +0200

    Added TYPO3 8 and PHP7 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.1.0dev',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
