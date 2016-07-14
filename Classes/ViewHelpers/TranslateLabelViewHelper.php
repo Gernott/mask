d93566eab48e1b94ab2e74dbc28e947f95e20b35
butu <bb@webprofil.at>
2016-07-14 15:39:24 +0200

    Adjusted PSR-2 Formatting

--- Classes/ViewHelpers/TranslateLabelViewHelper.php
+++ Classes/ViewHelpers/TranslateLabelViewHelper.php
@@ -15,27 +15,28 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
  * @package TYPO3
  * @subpackage mask
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- * @author Fabian Galinski fabian@sgalinski.de>
+ * @author Fabian Galinski <fabian@sgalinski.de>
  *
  */
-class TranslateLabelViewHelper extends AbstractViewHelper {
-	/**
-	 * The given key will be translated. If the result is empty, the key will be returned.
-	 *
-	 * @param string $key
-	 * @param string $extensionName
-	 * @return string
-	 */
-	public function render($key = NULL, $extensionName = NULL) {
-		if (empty($key) || strpos($key, 'LLL') > 0) {
-			return $key;
-		}
+class TranslateLabelViewHelper extends AbstractViewHelper
+{
 
-		$request = $this->renderingContext->getControllerContext()->getRequest();
-		$extensionName = $extensionName === NULL ? $request->getControllerExtensionName() : $extensionName;
-		$result = LocalizationUtility::translate($key, $extensionName);
-		return (empty($result) ? $key : $result);
-	}
+    /**
+     * The given key will be translated. If the result is empty, the key will be returned.
+     *
+     * @param string $key
+     * @param string $extensionName
+     * @return string
+     */
+    public function render($key = NULL, $extensionName = NULL)
+    {
+        if (empty($key) || strpos($key, 'LLL') > 0) {
+            return $key;
+        }
+
+        $request = $this->renderingContext->getControllerContext()->getRequest();
+        $extensionName = $extensionName === NULL ? $request->getControllerExtensionName() : $extensionName;
+        $result = LocalizationUtility::translate($key, $extensionName);
+        return (empty($result) ? $key : $result);
+    }
 }
-
-?>
