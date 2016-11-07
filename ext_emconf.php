6b63b975d4ad49e3dc67cd3ceffc18e239c74f6c
Harry Glatz <glatz@analog.de>
2016-11-07 16:04:12 +0100

    [TASK] Set dependency to TYPO3 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.1.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.2.99',
+            'typo3' => '7.6.0-8.4.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
