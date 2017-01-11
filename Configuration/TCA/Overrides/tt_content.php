927735aa7a2cd11db9b07d8a8cdf9f1c6bbea855
Manfred Rutschmann <manfred@rutschmann.biz>
2017-01-11 11:25:09 +0100

    [BUGFIX]: TCA Exception for content elements in TYPO3 8.5.x. Moved dynamic created TCA from ext_tables.php to TCA/Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,3 +10,19 @@ if (!empty($GLOBALS['TCA']['tt_content']['columns']['CType']['config']['itemsPro
 // and set mask itemsProcFuncs
 $GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['itemsProcFunc'] = 'MASK\Mask\ItemsProcFuncs\ColPosList->itemsProcFunc';
 $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['itemsProcFunc'] = 'MASK\Mask\ItemsProcFuncs\CTypeList->itemsProcFunc';
+
+$storageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('MASK\\Mask\\Domain\\Repository\\StorageRepository');
+$configuration = $storageRepository->load();
+
+if (!empty($configuration)) {
+
+   $tcaCodeGenerator = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('MASK\\Mask\\CodeGenerator\\TcaCodeGenerator');
+
+   // Generate TCA for Content-Elements
+   $contentColumns = $tcaCodeGenerator->generateFieldsTca($configuration["tt_content"]["tca"]);
+   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $contentColumns);
+   $tcaCodeGenerator->setElementsTca($configuration["tt_content"]["elements"]);
+
+   // Generate TCA for Inline-Fields
+   $tcaCodeGenerator->setInlineTca($configuration);
+}
