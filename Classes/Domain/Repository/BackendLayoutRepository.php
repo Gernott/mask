14587862c979320b20b081b1faa27fdcf8c34f8e
Markus Poerschke <poerschke@cvc.digital>
2018-08-18 22:49:53 +0200

    Replace TYPO3_DB with Doctrine DBAL in BackenLayoutRepository

--- Classes/Domain/Repository/BackendLayoutRepository.php
+++ Classes/Domain/Repository/BackendLayoutRepository.php
@@ -27,6 +27,9 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  * ************************************************************* */
 
+use Doctrine\DBAL\FetchMode;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Page\PageRepository;
 
@@ -101,10 +104,20 @@ class BackendLayoutRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
      */
     public function findIdentifierByPid($pid)
     {
-        $sql = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-            "backend_layout, backend_layout_next_level", "pages", "uid = " . $pid
-        );
-        $data = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($sql);
+        /** @var Connection $connection */
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('pages');
+
+        $queryBuilder = $connection->createQueryBuilder();
+        $queryBuilder
+            ->from('pages')
+            ->select('backend_layout', 'backend_layout_next_level', 'uid')
+            ->where('uid = :pid')
+            ->setParameter('pid', $pid);
+
+        $statement = $queryBuilder->execute();
+        $data = $statement->fetch(FetchMode::ASSOCIATIVE);
+        $statement->closeCursor();
+
         $backend_layout = $data["backend_layout"];
         $backend_layout_next_level = $data["backend_layout_next_level"];
         if ($backend_layout !== "") { // If backend_layout is set on current page
