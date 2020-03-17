685c19f4f77e4f6bd4785aabc6fe8f6803001822
Benjamin Butschell <bb@webprofil.at>
2020-03-17 16:49:24 +0100

    [CLEANUP] cleanup, refactoring and speed enhancements for typoscript condition maskBeLayout

--- Classes/ExpressionLanguage/MaskFunctionsProvider.php
+++ Classes/ExpressionLanguage/MaskFunctionsProvider.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace MASK\Mask\ExpressionLanguage;
 
@@ -9,7 +10,6 @@ use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
-use TYPO3\CMS\Core\Domain\Repository\PageRepository;
 use TYPO3\CMS\Core\ExpressionLanguage\RequestWrapper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -32,72 +32,27 @@ class MaskFunctionsProvider implements ExpressionFunctionProviderInterface
     protected function maskBeLayout(): ExpressionFunction
     {
         return new ExpressionFunction('maskBeLayout', static function ($param) {
-
         }, static function ($arguments, $param = null) {
 
-            /** @var RequestWrapper $request */
-            $request = $arguments['request'];
-            $requestParameters = $request->getQueryParams();
-
-
-            $uid = null;
             $layout = (string)$param;
-            // get current page uid:
-            if (is_array($requestParameters['edit']['pages'])) { // after saving page
-                $uid = (int)key($requestParameters['edit']['pages']);
-            } elseif ($GLOBALS['SOBE']->editconf['pages']) { // after opening pages
-                $uid = (int)key($GLOBALS['SOBE']->editconf['pages']);
-            } else {
-                if (!empty($GLOBALS['_SERVER']['HTTP_REFERER'])) {
-                    $url = $GLOBALS['_SERVER']['HTTP_REFERER'];
-                    $queryString = parse_url($url, PHP_URL_QUERY);
-                    $result = [];
-                    parse_str($queryString, $result);
-                    if ($result['id']) {
-                        $uid = (int)$result['id'];
-                    }
-                }
+            $backend_layout = (string)$arguments['page']['backend_layout'];
+            $backend_layout_next_level = (string)$arguments['page']['backend_layout_next_level'];
+
+            // If backend_layout is set on current page
+            if (!empty($backend_layout)) {
+                return in_array($backend_layout, [$layout, 'pagets__' . $layout], true);
             }
 
-            if ($uid) {
-                /** @var Connection $connection */
-                $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('pages');
-                $query = $connection->createQueryBuilder();
-                /** @var DeletedRestriction $deletedRestriction */
-                $deletedRestriction = GeneralUtility::makeInstance(DeletedRestriction::class);
-                $query->getRestrictions()->removeAll()->add($deletedRestriction);
-                $data = $query->select(
-                    'backend_layout',
-                    'backend_layout_next_level'
-                )->from('pages')
-                    ->where(
-                        $query->expr()->eq('uid', $uid)
-                    )->execute()
-                    ->fetch(FetchMode::ASSOCIATIVE);
+            // If backend_layout_next_level is set on current page
+            if (!empty($backend_layout_next_level)) {
+                return in_array($backend_layout_next_level, [$layout, 'pagets__' . $layout], true);
+            }
 
-                $backend_layout = (string)$data['backend_layout'];
-                $backend_layout_next_level = (string)$data['backend_layout_next_level'];
-
-                if (!empty($backend_layout)) { // If backend_layout is set on current page
-                    return in_array($backend_layout, [$layout, 'pagets__' . $layout], true);
-                }
-
-                if (!empty($backend_layout_next_level)) { // If backend_layout_next_level is set on current page
-                    return in_array($backend_layout_next_level, [$layout, 'pagets__' . $layout], true);
-                }
-
-                // If backend_layout and backend_layout_next_level is not set on current page, check backend_layout_next_level on rootline
-                $sysPage = GeneralUtility::makeInstance(PageRepository::class);
-                try {
-                    $rootline = $sysPage->getRootLine($uid, '');
-                } catch (\Exception $e) {
-                    $rootline = [];
-                }
-                foreach ($rootline as $page) {
-                    if (in_array((string)$page['backend_layout_next_level'], [$layout, 'pagets__' . $layout],
-                        true)) {
-                        return true;
-                    }
+            // If backend_layout and backend_layout_next_level is not set on current page, check backend_layout_next_level on rootline
+            foreach ($arguments['tree']->rootLine as $page) {
+                if (in_array((string)$page['backend_layout_next_level'], [$layout, 'pagets__' . $layout],
+                    true)) {
+                    return true;
                 }
             }
             return false;
