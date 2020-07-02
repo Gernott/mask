12608c8695b399b206cb50dd868673c3c40e02d8
Nikita Hovratov <nikita.h@live.de>
2020-07-02 11:25:35 +0200

    Remove unrecommended TYPO3_MODE check and cleanup code in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,35 +1,29 @@
 <?php
+
 defined('TYPO3_MODE') or die();
 
-(function ($extkey) {
-
-    if (TYPO3_MODE === 'BE') {
-
-        /**
-         * Registers a Backend Module
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            $extkey,
-            'tools', // Make module a submodule of 'admin'
-            'mask', // Submodule key
-            'bottom', // Position
-            [
-            	\MASK\Mask\Controller\WizardController::class => 'list,createMissingFolders',
-                \MASK\Mask\Controller\WizardContentController::class => 'list, new, create, edit, update, delete, purge, generate, showHtml, createMissingFolders, hide, activate, createHtml',
-                \MASK\Mask\Controller\WizardPageController::class => 'list, new, create, edit, update, delete, showHtml',
-            ], [
-                'access' => 'admin',
-                'icon' => 'EXT:' . $extkey . '/Resources/Public/Icons/module-mask_wizard.svg',
-                'labels' => 'LLL:EXT:' . $extkey . '/Resources/Private/Language/locallang_mask.xlf',
-            ]
-        );
-    }
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extkey, 'Configuration/TypoScript', 'Mask');
+(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'Mask',
+        'tools',
+        'mask',
+        'bottom',
+        [
+            \MASK\Mask\Controller\WizardController::class => 'list, createMissingFolders',
+            \MASK\Mask\Controller\WizardContentController::class => 'list, new, create, edit, update, delete, purge, generate, showHtml, createMissingFolders, hide, activate, createHtml',
+            \MASK\Mask\Controller\WizardPageController::class => 'list, new, create, edit, update, delete, showHtml',
+        ],
+        [
+            'access' => 'admin',
+            'icon' => 'EXT:mask/Resources/Public/Icons/module-mask_wizard.svg',
+            'labels' => 'LLL:EXT:mask/Resources/Private/Language/locallang_mask.xlf',
+        ]
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mask', 'Configuration/TypoScript', 'Mask');
 
     // include css for styling of backend preview of mask content elements
     $TBE_STYLES['skins']['mask']['name'] = 'mask';
-    $TBE_STYLES['skins']['mask']['stylesheetDirectories'][] = 'EXT:' . $extkey . '/Resources/Public/Styles/Backend/';
-    //$TBE_STYLES['skins']['mask']['stylesheetDirectories'][] = "/" . $settings["backend"];
+    $TBE_STYLES['skins']['mask']['stylesheetDirectories'][] = 'EXT:mask/Resources/Public/Styles/Backend/';
 
     $storageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(MASK\Mask\Domain\Repository\StorageRepository::class);
     $configuration = $storageRepository->load();
@@ -40,5 +34,4 @@ defined('TYPO3_MODE') or die();
         // allow all inline tables on standard pages
         $tcaCodeGenerator->allowInlineTablesOnStandardPages($configuration);
     }
-
-})('mask');
+})();
