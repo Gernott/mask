f1d54d7858f0446f76cc19d65f889ca9d80623c0
Nikita Hovratov <nikita.h@live.de>
2020-09-13 16:41:57 +0200

    [TASK] Improve mask checkboxes UI
    - Checkboxes are replaced with TYPO3 core toggle checkboxes.
    - The tca config option is displayed under the title as code tag.
    - Removed some deprecated configuration options.

--- Classes/ViewHelpers/CheckboxViewHelper.php
+++ Classes/ViewHelpers/CheckboxViewHelper.php
@@ -30,9 +30,13 @@ class CheckboxViewHelper extends AbstractViewHelper
         $this->registerArgument('name', 'string', 'name of checkbox', true);
         $this->registerArgument('editMode', 'bool', 'edit mode', true);
         $this->registerArgument('translationKey', 'string', 'Translation for Label', true);
-        $this->registerArgument('invert', 'bool', 'Invert checkbox', false, false);
-        $this->registerArgument('default', 'int', 'default value', false, 0);
-        $this->registerArgument('value', 'int', 'default value');
+        $this->registerArgument('invert', 'bool', 'Invert checkbox', false, 0);
+        $this->registerArgument('default', 'string', 'default on or off?', false, 'off');
+        $this->registerArgument('valueOff', 'string', 'off value', false, '0');
+        $this->registerArgument('valueOn', 'string', 'on value', false, '1');
+        $this->registerArgument('value', 'string', 'current value');
+        $this->registerArgument('eval', 'bool', 'is eval field');
+        $this->registerArgument('link', 'bool', 'is link field');
     }
 
     public static function renderStatic(
@@ -41,33 +45,50 @@ class CheckboxViewHelper extends AbstractViewHelper
         RenderingContextInterface $renderingContext
     ) {
         $name = $arguments['name'];
+        $eval = $arguments['eval'];
+        $link = $arguments['link'];
+        $path = '[storage][tca][--index--]';
+        if ($eval || $link) {
+            $path = '[dummy][--index--]';
+        }
         $hashName = str_replace(['[', ']'], '', $name);
         $value = $arguments['value'];
+        $valueOn = $arguments['valueOn'];
+        $valueOff = $arguments['valueOff'];
         $editMode = $arguments['editMode'];
         $invert = $arguments['invert'] ? ' checkbox-invert' : '';
 
         // Default values for new fields
         $default = $arguments['default'];
-        if ($default === 1) {
-            $checked = !$editMode || $value;
+        if ((int)($default === 'on') xor (int)$invert) {
+            $checked = !$editMode || ($value === $valueOn);
         } else {
-            $checked = $editMode && $value;
+            $checked = $editMode && ($value === $valueOn);
         }
 
         $checkedAttr = $checked ? ' checked' : '';
-        $checkedValue = $checked ? '1' : '0';
+        $checkedValue = $checked ? $valueOn : $valueOff;
 
         $random = random_int(0, mt_getrandmax());
         $hash = $hashName . substr(md5($random . $name), 0, 9);
         $label = LocalizationUtility::translate($arguments['translationKey'], 'mask');
 
+        $switchFunction = "this.checked?'$valueOn':'$valueOff';";
+        $classTag = '';
+        if ($eval) {
+            $classTag = 'class="tx_mask_fieldcontent_eval"';
+        }
+        if ($link) {
+            $classTag = 'class="tx_mask_fieldcontent_link"';
+        }
+
         $html = [];
         $html[] = '<div class="js-update-id checkbox checkbox-type-toggle' . $invert . '">';
-        $html[] = '<input id="' . $hash . '" type="checkbox" class="checkbox-input" value="1" onclick="document.getElementById(\'' . $hash . '_hidden\').value=this.checked?1:0;"' . $checkedAttr . '>';
+        $html[] = '<input id="' . $hash . '" type="checkbox" class="checkbox-input" value="1" onclick="document.getElementById(\'' . $hash . '_hidden\').value=' . $switchFunction . '"' . $checkedAttr . '>';
         $html[] = '<label class="checkbox-label" for="' . $hash . '">';
         $html[] = '<span class="checkbox-label-text">' . $label . '</span>';
         $html[] = '</label>';
-        $html[] = '<input id="' . $hash . '_hidden" type="hidden" name="tx_mask_tools_maskmask[storage][tca][--index--][config]' . $name . '" value="' . $checkedValue . '"/>';
+        $html[] = '<input id="' . $hash . '_hidden" ' . $classTag .' type="hidden" name="tx_mask_tools_maskmask' . $path . $name . '" value="' . $checkedValue . '"/>';
         $html[] = '</div>';
 
         return implode("\r\n", $html);
