815dc17817e02c5472928298207fe33e885e3c77
Nikita Hovratov <nikita.h@live.de>
2021-10-12 16:31:35 +0200

    [BUGFIX] Remove wrong properties in Classes.php

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 
 return [
     MASK\Mask\Domain\Model\BackendLayout::class => [
-        'tableName' => 'backend_layout',
-        'properties' => [
-            'uid' => [
-                'fieldname' => 'uid'
-            ],
-            'title' => [
-                'fieldname' => 'title'
-            ]
-        ]
+        'tableName' => 'backend_layout'
     ]
 ];
