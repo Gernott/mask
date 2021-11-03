b8a7ec008614b2d41be6553c335e78b2231c0291
Nikita Hovratov <nikita.h@live.de>
2021-11-03 10:11:43 +0100

    [BUGFIX] Use correct table and CType for adding relations
    InlineHelper->getInlineElements used the field name as the table name
    for the child relations. This does only work for inline fields, because
    the field name and table are the same. Instead, the provided child
    table name must be used.
    
    The same goes for the cType. An inline field of type tt_content has
    probably another cType than the parent. This can be fetched from the
    data row.
    
    Fixes: #439

--- Classes/Helper/InlineHelper.php
+++ Classes/Helper/InlineHelper.php
@@ -285,8 +285,12 @@ class InlineHelper
 
         foreach ($elements as $key => $element) {
             if ($element) {
-                $this->addIrreToData($element, $name, $cType);
-                $this->addFilesToData($element, $name);
+                $childCType = $cType;
+                if ($childTable === 'tt_content') {
+                    $childCType = $element['CType'];
+                }
+                $this->addIrreToData($element, $childTable, $childCType);
+                $this->addFilesToData($element, $childTable);
                 $elements[$key] = $element;
             }
         }
