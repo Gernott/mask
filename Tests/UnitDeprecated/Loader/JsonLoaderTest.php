fdafa01dbdeffff9b1ab2d88ccf310c9fbb6900b
Nikita Hovratov <nikita.h@live.de>
2021-12-18 15:44:45 +0100

    [BUGFIX] Resolve old rte type format
    Since the field type resolving refactoring, a
    breaking change has been introduced for resolving
    the rte type via element options.
    
    This was good and needed, but the breaking change
    should not be introduced in a minor version.
    
    This patch adds a compatibility layer in the
    JsonLoader, to be able to still resolve to rte.
    
    (cherry picked from commit 7dcad6b4a13a7dd0122b5e762168ab6228d58779)

---
+++ Tests/UnitDeprecated/Loader/JsonLoaderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MASK\Mask\Tests\UnitDeprecated\Loader;
+
+use MASK\Mask\Enumeration\FieldType;
+use MASK\Mask\Loader\JsonLoader;
+use MASK\Mask\Tests\Unit\ConfigurationLoader\FakeConfigurationLoader;
+use MASK\Mask\Tests\Unit\PackageManagerTrait;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class JsonLoaderTest extends UnitTestCase
+{
+    use PackageManagerTrait;
+    /**
+     * @test
+     */
+    public function legacyRteFormatCompatibilityLayerWorks(): void
+    {
+        $this->registerPackageManager();
+
+        $jsonLoader = new JsonLoader(
+            [
+                'json' => 'EXT:mask/Tests/UnitDeprecated/Fixtures/Configuration/legacyRte.json',
+            ],
+            new FakeConfigurationLoader()
+        );
+
+        $tableDefinitionCollection = $jsonLoader->load();
+        self::assertTrue($tableDefinitionCollection->loadField('tt_content', 'tx_mask_rte')->type->equals(FieldType::RICHTEXT), 'Field tx_mask_rte is not a richtext.');
+    }
+}
