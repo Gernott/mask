f7dec5585868f2334230073c1d55497ff6e8aa51
Nitori (Sani) <l.soendergaard@lichtflut-medien.de>
2022-02-18 17:27:35 +0100

    [BUGFIX] Make richtext field type searchable (#471)
    (cherry picked from commit 988343fbd192bb914b3629e6c0ce52002097baf3)

--- Classes/Enumeration/FieldType.php
+++ Classes/Enumeration/FieldType.php
@@ -54,7 +54,7 @@ final class FieldType extends Enumeration
 
     public function isSearchable(): bool
     {
-        return in_array($this->value, [self::STRING, self::TEXT], true);
+        return in_array($this->value, [self::STRING, self::TEXT, self::RICHTEXT], true);
     }
 
     public function isRenderable(): bool
