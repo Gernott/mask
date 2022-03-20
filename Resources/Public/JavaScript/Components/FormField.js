bc31249e965e8d095f7bb6e31acb32d4c3f17ac4
Nikita Hovratov <nikita.h@live.de>
2022-03-20 16:45:31 +0100

    [BUGFIX] Fix available condition for FormField

--- Resources/Public/JavaScript/Components/FormField.js
+++ Resources/Public/JavaScript/Components/FormField.js
@@ -75,7 +75,6 @@ define([
             if (typeof this.tcaFields[this.tcaKey] === 'undefined') {
               return {available: false};
             }
-            this.tcaFields[this.tcaKey].available = true;
             if (this.global.activeField.name in this.tcaFields[this.tcaKey]) {
               return this.tcaFields[this.tcaKey][this.global.activeField.name];
             } else if ('other' in this.tcaFields[this.tcaKey]) {
@@ -132,7 +131,7 @@ define([
           }
         },
         template: `
-          <div v-if="field.available" :class="['form-group', 'col-sm-12 col-xl-' + column, {'has-error': hasError}]">
+          <div v-if="field.available !== false" :class="['form-group', 'col-sm-12 col-xl-' + column, {'has-error': hasError}]">
             <label class="t3js-formengine-label" :for="tcaKey">
                 {{ field.label }}
             </label>
