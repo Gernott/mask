49825cea91666e35498afcbdb657617598b4b0ae
Nikita Hovratov <nikita.h@live.de>
2022-04-16 15:12:02 +0200

    [TASK] Rewrite HtmlCodeGenerator to use API objects

--- Classes/CodeGenerator/HtmlCodeGenerator.php
+++ Classes/CodeGenerator/HtmlCodeGenerator.php
@@ -17,6 +17,9 @@ declare(strict_types=1);
 
 namespace MASK\Mask\CodeGenerator;
 
+use MASK\Mask\Definition\ElementTcaDefinition;
+use MASK\Mask\Definition\TableDefinitionCollection;
+use MASK\Mask\Definition\TcaFieldDefinition;
 use MASK\Mask\Enumeration\FieldType;
 use MASK\Mask\Loader\LoaderInterface;
 use MASK\Mask\Utility\AffixUtility;
@@ -38,6 +41,11 @@ class HtmlCodeGenerator
      */
     protected $indent = 4;
 
+    /**
+     * @var TableDefinitionCollection|null
+     */
+    protected $tableDefinitionCollection;
+
     public function __construct(LoaderInterface $loader)
     {
         $this->loader = $loader;
@@ -48,16 +56,16 @@ class HtmlCodeGenerator
      */
     public function generateHtml(string $elementKey, string $table): string
     {
-        $tableDefinitionCollection = $this->loader->load();
-        $element = $tableDefinitionCollection->loadElement($table, $elementKey);
+        $this->tableDefinitionCollection = $this->loader->load();
+        $element = $this->tableDefinitionCollection->loadElement($table, $elementKey);
 
-        if (!$element) {
+        if (!$element instanceof ElementTcaDefinition) {
             return '';
         }
 
         $html = [];
         foreach ($element->getRootTcaFields() as $field) {
-            $part = $this->generateFieldHtml($field->fullKey, $elementKey, $table);
+            $part = $this->generateFieldHtml($field, $element, $table);
             if ($part !== '') {
                 $html[] = $part;
             }
@@ -71,83 +79,81 @@ class HtmlCodeGenerator
     /**
      * Generates HTML for a field
      */
-    protected function generateFieldHtml(string $fieldKey, string $elementKey, string $table, string $datafield = 'data', int $depth = 0): string
+    protected function generateFieldHtml(TcaFieldDefinition $field, ElementTcaDefinition $element, string $table, string $datafield = 'data', int $depth = 0): string
     {
-        $tableDefinitionCollection = $this->loader->load();
         $html = [];
-        $fieldType = $tableDefinitionCollection->getFieldType($fieldKey, $table);
-        if (!$fieldType->isRenderable()) {
+        if (!$field->type->isRenderable()) {
             return '';
         }
-        switch ((string)$fieldType) {
+        switch ((string)$field->type) {
             case FieldType::SELECT:
-                if (($GLOBALS['TCA'][$table]['columns'][$fieldKey]['config']['foreign_table'] ?? '') !== '') {
-                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '_items}" as="' . $datafield . '_item' . '">';
+                if (($field->realTca['config']['foreign_table'] ?? '') !== '') {
+                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '_items}" as="' . $datafield . '_item' . '">';
                     $html[] = $this->drawWhitespace(1 + $depth) . '<div>{' . $datafield . '_item.uid}' . '</div>';
                     $html[] = $this->drawWhitespace(0 + $depth) . '</f:for>';
-                } elseif (($GLOBALS['TCA'][$table]['columns'][$fieldKey]['config']['renderType'] ?? '') === 'selectCheckBox') {
-                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '_items}" as="' . $datafield . '_item' . '">';
+                } elseif (($field->realTca['config']['renderType'] ?? '') === 'selectCheckBox') {
+                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '_items}" as="' . $datafield . '_item' . '">';
                     $html[] = $this->drawWhitespace(1 + $depth) . '<div>{' . $datafield . '_item}' . '</div>';
                     $html[] = $this->drawWhitespace(0 + $depth) . '</f:for>';
                 } else {
-                    $html[] = $this->drawWhitespace(0 + $depth) . $this->getVariable($datafield, $fieldKey);
+                    $html[] = $this->drawWhitespace(0 + $depth) . $this->getVariable($datafield, $field->fullKey);
                 }
                 break;
             case FieldType::CATEGORY:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '_items}" as="category">';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '_items}" as="category">';
                 $html[] = $this->drawWhitespace(1 + $depth) . '<div>{category.title}</div>';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:for>';
                 break;
             case FieldType::RADIO:
             case FieldType::CHECK:
-                $html[] = $this->drawWhitespace(0 + $depth) . $this->getVariable($datafield, $fieldKey);
+                $html[] = $this->drawWhitespace(0 + $depth) . $this->getVariable($datafield, $field->fullKey);
                 break;
             case FieldType::CONTENT:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="' . $datafield . '_item' . '">';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '}" as="' . $datafield . '_item' . '">';
                 $html[] = $this->drawWhitespace(2 + $depth) . '<f:cObject typoscriptObjectPath="lib.tx_mask.content">{' . $datafield . '_item.uid}</f:cObject>';
                 $html[] = $this->drawWhitespace(1 + $depth) . '</f:for>';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::DATE:
             case FieldType::TIMESTAMP:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . ' -> f:format.date(format: \'d.m.Y\')}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . ' -> f:format.date(format: \'d.m.Y\')}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::DATETIME:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . ' -> f:format.date(format: \'d.m.Y - H:i:s\')}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . ' -> f:format.date(format: \'d.m.Y - H:i:s\')}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::FILE:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="file">';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '}" as="file">';
                 $html[] = $this->drawWhitespace(2 + $depth) . '<f:image image="{file}" width="200" />';
                 $html[] = $this->drawWhitespace(2 + $depth) . '{file.description}';
                 $html[] = $this->drawWhitespace(1 + $depth) . '</f:for>';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::MEDIA:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="file">';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '}" as="file">';
                 $html[] = $this->drawWhitespace(2 + $depth) . '<f:media file="{file}" width="200" />';
                 $html[] = $this->drawWhitespace(2 + $depth) . '{file.description}';
                 $html[] = $this->drawWhitespace(1 + $depth) . '</f:for>';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::FLOAT:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . ' -> f:format.number(decimals: \'2\', decimalSeparator: \',\', thousandsSeparator: \'.\')}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . ' -> f:format.number(decimals: \'2\', decimalSeparator: \',\', thousandsSeparator: \'.\')}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::INLINE:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
                 $html[] = $this->drawWhitespace(1 + $depth) . '<ul>';
-                $html[] = $this->drawWhitespace(2 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '}" as="' . $datafield . '_item' . '">';
+                $html[] = $this->drawWhitespace(2 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '}" as="' . $datafield . '_item' . '">';
                 $html[] = $this->drawWhitespace(3 + $depth) . '<li>';
-                foreach ($tableDefinitionCollection->loadInlineFields($fieldKey, $elementKey) as $inlineField) {
-                    $html[] = $this->generateFieldHtml($inlineField->fullKey, $elementKey, $fieldKey, $datafield . '_item', 4 + $depth);
+                foreach ($this->tableDefinitionCollection->loadInlineFields($field->fullKey, $element->elementDefinition->key) as $inlineField) {
+                    $html[] = $this->generateFieldHtml($inlineField, $element, $field->fullKey, $datafield . '_item', 4 + $depth);
                 }
                 $html[] = $this->drawWhitespace(3 + $depth) . '</li>';
                 $html[] = $this->drawWhitespace(2 + $depth) . '</f:for>';
@@ -155,16 +161,16 @@ class HtmlCodeGenerator
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::PALETTE:
-                foreach ($tableDefinitionCollection->loadInlineFields($fieldKey, $elementKey) as $paletteField) {
-                    $part = $this->generateFieldHtml($paletteField->fullKey, $elementKey, $table, $datafield, $depth);
+                foreach ($this->tableDefinitionCollection->loadInlineFields($field->fullKey, $element->elementDefinition->key) as $paletteField) {
+                    $part = $this->generateFieldHtml($paletteField, $element, $table, $datafield, $depth);
                     if ($part !== '') {
                         $html[] = $part;
                     }
                 }
                 break;
             case FieldType::GROUP:
-                if (($GLOBALS['TCA'][$table]['columns'][$fieldKey]['config']['internal_type'] ?? '') !== 'folder') {
-                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $fieldKey . '_items}" as="' . $datafield . '_item' . '">';
+                if (($field->realTca['config']['internal_type'] ?? '') !== 'folder') {
+                    $html[] = $this->drawWhitespace(0 + $depth) . '<f:for each="{' . $datafield . '.' . $field->fullKey . '_items}" as="' . $datafield . '_item' . '">';
                     $html[] = $this->drawWhitespace(1 + $depth) . '<div>{' . $datafield . '_item.uid}' . '</div>';
                     $html[] = $this->drawWhitespace(0 + $depth) . '</f:for>';
                     break;
@@ -173,27 +179,27 @@ class HtmlCodeGenerator
             case FieldType::STRING:
             case FieldType::INTEGER:
             case FieldType::COLORPICKER:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . '}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . '}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::LINK:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '<f:link.typolink parameter="{' . $datafield . '.' . $fieldKey . '}"></f:link.typolink>';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '<f:link.typolink parameter="{' . $datafield . '.' . $field->fullKey . '}"></f:link.typolink>';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::RICHTEXT:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . ' -> f:format.html()}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . ' -> f:format.html()}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::TEXT:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $fieldKey . '}">';
-                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $fieldKey . ' -> f:format.nl2br()}';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<f:if condition="{' . $datafield . '.' . $field->fullKey . '}">';
+                $html[] = $this->drawWhitespace(1 + $depth) . '{' . $datafield . '.' . $field->fullKey . ' -> f:format.nl2br()}';
                 $html[] = $this->drawWhitespace(0 + $depth) . '</f:if>';
                 break;
             case FieldType::SLUG:
-                $html[] = $this->drawWhitespace(0 + $depth) . '<div id="{' . $datafield . '.' . $fieldKey . '}"></div>';
+                $html[] = $this->drawWhitespace(0 + $depth) . '<div id="{' . $datafield . '.' . $field->fullKey . '}"></div>';
                 break;
         }
         return implode("\n", $html);
