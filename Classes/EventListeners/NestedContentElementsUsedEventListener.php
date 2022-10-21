5b01b3d66d7469c9e280d876ba9444d572e006a6
Nikita Hovratov <nikita.h@live.de>
2022-10-21 22:02:49 +0200

    [TASK] Migrate content used hook to PSR-14 Event

---
+++ Classes/EventListeners/NestedContentElementsUsedEventListener.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace MASK\Mask\EventListeners;
+
+use TYPO3\CMS\Backend\View\Event\IsContentUsedOnPageLayoutEvent;
+
+/**
+ * Allow the usage of records in colPos 999 for Mask nested content elements.
+ */
+class NestedContentElementsUsedEventListener
+{
+    public function __invoke(IsContentUsedOnPageLayoutEvent $event): void
+    {
+        if ($event->getRecord()['colPos'] === 999) {
+            $event->setUsed(true);
+        }
+    }
+}
