2166fdee985604610a815cd39b704303599a8330
Nikita Hovratov <nikita.h@live.de>
2023-02-27 18:35:16 +0100

    [BUGFIX] Do not treat custom bodytext field as core bodytext field
    The fullKey should be check instead, so tx_mask_bodytext does not get
    its type removed.

--- Classes/Domain/Repository/StorageRepository.php
+++ Classes/Domain/Repository/StorageRepository.php
@@ -299,7 +299,7 @@ class StorageRepository implements SingletonInterface
                 }
             }
 
-            if ($fieldAdd['key'] === 'bodytext') {
+            if ($fieldAdd['fullKey'] === 'bodytext') {
                 $fieldAdd['bodytextTypeByElement'][$elementKey] = $fieldAdd['type'];
                 unset($fieldAdd['type']);
             }
