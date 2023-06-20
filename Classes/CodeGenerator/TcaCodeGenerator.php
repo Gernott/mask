23d68c591a08b0154e856d31d0886df4326afc66
Albrecht Köhnlein <github@bestof1983.de>
2023-06-20 16:20:56 +0200

    Show correct field "l10n_parent" in language palette (#564)

--- Classes/CodeGenerator/TcaCodeGenerator.php
+++ Classes/CodeGenerator/TcaCodeGenerator.php
@@ -801,7 +801,7 @@ class TcaCodeGenerator
             'palettes' => [
                 'language' => [
                     'showitem' => '
-                        sys_language_uid;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:sys_language_uid_formlabel,l18n_parent
+                        sys_language_uid;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:sys_language_uid_formlabel,l10n_parent
                     ',
                 ],
                 'hidden' => [
