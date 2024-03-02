e4c376678666fd8b0248fa8fbe5db5c5ab8051d4
Nikita Hovratov <nikita.h@live.de>
2024-03-02 15:45:49 +0100

    [TASK] Remove old module registration

--- ext_tables.php
+++ ext_tables.php
@@ -2,25 +2,6 @@
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-    'tools',
-    'mask',
-    'after:extensionmanager',
-    null,
-    [
-        'routeTarget' => \MASK\Mask\Controller\MaskController::class . '::mainAction',
-        'access' => 'group,user',
-        'name' => 'tools_mask',
-        'icon' => 'EXT:mask/Resources/Public/Icons/module-mask_wizard.svg',
-        'labels' => 'LLL:EXT:mask/Resources/Private/Language/locallang_mask.xlf',
-    ]
-);
-
-// Include css for styling of backend preview of mask content elements
-if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 12) {
-    $GLOBALS['TBE_STYLES']['skins']['mask']['stylesheetDirectories']['mask'] = 'EXT:mask/Resources/Public/Styles/Backend/';
-}
-
 (static function () {
     // Allow all inline tables on standard pages
     $tables = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\MASK\Mask\Loader\LoaderRegistry::class)->loadActiveDefinition();
