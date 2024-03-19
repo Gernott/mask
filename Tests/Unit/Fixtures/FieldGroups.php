4ba76c99a794968a6c97a2ebafda9fa09d5032d4
Nikita Hovratov <nikita.h@live.de>
2024-03-19 15:36:50 +0100

    [TASK] Fix cgl

--- Tests/Unit/Fixtures/FieldGroups.php
+++ Tests/Unit/Fixtures/FieldGroups.php
@@ -3,28 +3,28 @@
 use MASK\Mask\Enumeration\FieldType;
 
 return [
-        FieldType::STRING => 'input',
-        FieldType::INTEGER => 'input',
-        FieldType::FLOAT => 'input',
-        FieldType::LINK => 'input',
+    FieldType::STRING => 'input',
+    FieldType::INTEGER => 'input',
+    FieldType::FLOAT => 'input',
+    FieldType::LINK => 'input',
 
-        FieldType::TEXT => 'text',
-        FieldType::RICHTEXT => 'text',
+    FieldType::TEXT => 'text',
+    FieldType::RICHTEXT => 'text',
 
-        FieldType::DATE => 'date',
-        FieldType::DATETIME => 'date',
-        FieldType::TIMESTAMP => 'date',
+    FieldType::DATE => 'date',
+    FieldType::DATETIME => 'date',
+    FieldType::TIMESTAMP => 'date',
 
-        FieldType::CHECK => 'choice',
-        FieldType::RADIO => 'choice',
-        FieldType::SELECT => 'choice',
-        FieldType::GROUP => 'choice',
+    FieldType::CHECK => 'choice',
+    FieldType::RADIO => 'choice',
+    FieldType::SELECT => 'choice',
+    FieldType::GROUP => 'choice',
 
-        FieldType::FILE => 'repeating',
-        FieldType::INLINE => 'repeating',
-        FieldType::CONTENT => 'repeating',
+    FieldType::FILE => 'repeating',
+    FieldType::INLINE => 'repeating',
+    FieldType::CONTENT => 'repeating',
 
-        FieldType::TAB => 'structure',
-        FieldType::PALETTE => 'structure',
-        FieldType::LINEBREAK => 'structure',
+    FieldType::TAB => 'structure',
+    FieldType::PALETTE => 'structure',
+    FieldType::LINEBREAK => 'structure',
 ];
