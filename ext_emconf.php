6fbe44dac8a5b379c737bbff81ef61533038fc2a
Nikita Hovratov <nikita.h@live.de>
2024-11-15 10:38:10 +0100

    [TASK] Remove TYPO3 v12 support for now

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,9 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'fluid' => '12.4.0-13.4.99',
-            'fluid_styled_content' => '12.4.0-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
+            'fluid' => '13.4.0-13.4.99',
+            'fluid_styled_content' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
